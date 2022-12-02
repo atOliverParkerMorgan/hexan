45327b9bd81026d161797e731cf3026f7787d04e
Oliver <oliver.parker.morgan@gmail.com>
2022-12-02 19:50:40 +0100

    setting HOSTNAME back to localhost for testing branch

--- dist/client_logic/client_create_game.js
+++ dist/client_logic/client_create_game.js
@@ -5,7 +5,7 @@ var REQUEST_TYPES = {
 };
 var interval_id_timer;
 var interval_id_match_request;
-export var URL = "hexan0.herokuapp.com";
+export var HOST_NAME = "hexan0.herokuapp.com";
 function settings_logic_init() {
     // slider logic
     var args = [400, 900, 1225, 1600, 2025, 2500];
@@ -88,7 +88,7 @@ function settings_logic_init() {
             return;
         //client_socket.send_data("create_game_with_ai", nick);
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "https://".concat(URL, ":8000"), true);
+        xhr.open("POST", "https://".concat(HOST_NAME, ":8000"), true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         console.log("nick_name: ".concat(nick_name, " map_size: ").concat(map_size, " game_mode: ").concat(game_mode, " request_type: ").concat(REQUEST_TYPES.GENERATE_PLAYER_TOKEN));
         xhr.send(JSON.stringify({
@@ -127,7 +127,7 @@ function settings_logic_init() {
 // ask if there server has a match
 function request_match_status_update(player_token, nick_name, map_size, game_mode) {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://".concat(URL, ":8000"), true);
+    xhr.open("POST", "https://".concat(HOST_NAME, ":8000"), true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
         nick_name: nick_name,
