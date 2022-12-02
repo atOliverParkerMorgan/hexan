86c93b620f13fb43dbaaaed7ab8260c346746a75
Oliver <oliver.parker.morgan@gmail.com>
2022-12-02 17:46:12 +0100

    changing http  => to https

--- dist/client_logic/client_create_game.js
+++ dist/client_logic/client_create_game.js
@@ -5,7 +5,7 @@ var REQUEST_TYPES = {
 };
 var interval_id_timer;
 var interval_id_match_request;
-export var URL = "hexan0.herokuapp.com";
+export var URL = "localhost";
 function settings_logic_init() {
     // slider logic
     var args = [400, 900, 1225, 1600, 2025, 2500];
